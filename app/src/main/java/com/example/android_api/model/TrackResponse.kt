ba344e53649dabb9d41a1f1e488711d4c1d67abe
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2022-04-20 09:25:36 +0100

    Added the model, view and common

--- app/src/main/java/com/example/android_api/model/TrackResponse.kt
+++ app/src/main/java/com/example/android_api/model/TrackResponse.kt
@@ -1,4 +1,18 @@
 package com.example.android_api.model
 
-class TrackResponse {
-}
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import java.lang.reflect.Array
+//import java.util.ArrayList
+import java.io.Serializable
+
+class TrackList: ArrayList<TrackResponse>()
+
+@Parcelize
+ data class TrackResponse(
+            val image: String,
+            val collectionName: String,
+            val trackName: String,
+            val trackPrice: Float,
+            val currency: String
+): Parcelable
